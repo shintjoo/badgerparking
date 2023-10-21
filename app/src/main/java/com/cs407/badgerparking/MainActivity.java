f0a0c3f3967a612f05f6cfa6d8267e2aa65e5ce9
NLibenzon <nicolas.libenzon@gmail.com>
2023-10-21 12:07:10 -0500

    added park button and some project 4 location code - nick

--- app/src/main/java/com/cs407/badgerparking/MainActivity.java
+++ app/src/main/java/com/cs407/badgerparking/MainActivity.java
@@ -1,14 +1,88 @@
 package com.cs407.badgerparking;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    LocationManager locationManager;
+    LocationListener locationListener;
+
+    //I took the code from project 4's location manager, i just wanted to move it out of onCreate - nick
+    public void instantiateLocationServices(){
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+//                updateLocationInfo(location);
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle){
+
+            }
+            @Override
+            public void onProviderEnabled(String s){
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s){
+
+            }
+        };
+
+        if (Build.VERSION.SDK_INT < 23) {
+            startListening();
+        }
+        else{
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
+            else {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+                Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                if (location!=null){
+//                    updateLocationInfo(location);
+                }
+            }
+        }
+    }
+
+    //project 4 location code
+    public void startListening(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        }
+    }
+
+    //project 4 location code
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            startListening();
+        }
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        instantiateLocationServices();
     }
 }
