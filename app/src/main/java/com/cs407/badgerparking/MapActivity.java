7a9037c7b501a8eef2720fa8aa6558577cbb9c27
NLibenzon <nicolas.libenzon@gmail.com>
2023-10-22 17:44:55 -0500

    added activity views for all page views

---
+++ app/src/main/java/com/cs407/badgerparking/MapActivity.java
@@ -0,0 +1,14 @@
+package com.cs407.badgerparking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
